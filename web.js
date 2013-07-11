ff0899fa550332c30c7f3db115159f981a733bed
Ubuntu <ubuntu@ip-172-31-38-95.us-west-2.compute.internal>
2013-07-11 14:59:25 +0000

    Step 7

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+    var buf = new Buffer(data);  
+    Response.Send(buf.toString('utf8'));
+  }    
 });
 
 var port = process.env.PORT || 5000;
